e4dde8def0c4ec03d8823389e3a331ae0203448e
Simeon Schaub <simeondavidschaub99@gmail.com>
2020-08-16 12:50:14 +0200

    wip

--- src/JuliaMono.jl
+++ src/JuliaMono.jl
@@ -2,9 +2,34 @@ module JuliaMono
 
 export install
 
+import Pkg
 using Pkg.Artifacts
 
 const TTF_FILES = artifact"JuliaMono"
+const ARTIFACTS_TOML = find_artifacts_toml(@__LINE__)
+
+function save_install_location!(install_location)
+    git_tree_sha1 = create_artifact() do artifact_dir
+        open(joinpath(artifact_dir, "INSTALL_LOCATION"), "w") do io
+            println(io, install_location)
+        end
+    end
+
+    bind_artifact!(ARTIFACTS_TOML, "INSTALL_LOCATION", git_tree_sha_1)
+end
+
+function unsave_install_location!()
+    artifact_dir = ensure_artifact_installed("INSTALL_LOCATION", ARTIFACTS_TOML)
+
+    install_location = open(joinpath(artifact_dir, "INSTALL_LOCATION")) do io
+        readline(io)
+    end
+    
+    remove_artifact!(artifact_hash("INSTALL_LOCATION", ARTIFACTS_TOML))
+    unbind_artifact!(ARTIFACTS_TOML, "INSTALL_LOCATION")
+
+    return install_location
+end
 
 function install()
     for i in readdir(TTF_FILES)
